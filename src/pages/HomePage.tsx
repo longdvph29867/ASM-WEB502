e7d0d39a4691305e793dd66890a1d83f54be5932
longdv ph29867 <emchuaroinhe@gmail.com>
2023-11-15 22:20:00 +0700

    update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import Banner from "../components/Banner";
 import Banner2 from "../components/Banner2";
 import HowItWork from "../components/HowItWork";
 import ProductsList from "../components/ProductsList";
+import ShoppingFeatures from "../components/ShoppingFeatures";
 
 const HomePage = () => {
   return (
@@ -14,6 +15,7 @@ const HomePage = () => {
       <Banner2 />
       <ProductsList gender="female" />
       <ProductsList gender="male" />
+      <ShoppingFeatures className="container mx-auto grid grid-cols-4 mt-10" />
     </div>
   );
 };
